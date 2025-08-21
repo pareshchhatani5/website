703e7f36bc43dfe6d9a02fbcd74b1460ea8bc604
pareshchhatani5 <paresh.chhatani5@gmail.com>
2025-08-21 15:11:07 +0530

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import path from 'path'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss()
   ],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./"),
-    },
-  },
-  optimizeDeps: {
-    include: ['react', 'react-dom']
-  },
   build: {
     rollupOptions: {
       output: {
